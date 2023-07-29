6d7f9efdb63c1a347b182731c4c0f77588ee5983
LuizFelippyDev <128524115+Luizfelippy@users.noreply.github.com>
2023-07-29 12:34:26 -0300

    correções e inserção de novos elementos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form-valid');
+const form = document.getElementById('form-valid')
 
 function validarCampo(valorA, valorB) {
     return valorA < valorB;
@@ -7,19 +7,30 @@ function validarCampo(valorA, valorB) {
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const valorA = document.getElementById('campoA');
-    const valorB = document.getElementById('campoB');
-    const mensagemSucesso = `É valido: ${valorB.value} é maior que ${valorA.value}`;
+    const valorA = parseFloat(document.getElementById('campoA').value);
+    const valorB = parseFloat(document.getElementById('campoB').value);
+    const mensagemSucesso = `É válido: <b>${valorB}</b> é maior que <b>${valorA}</b>!`;
+    const mensagemErro = `Não é valido: <b>${valorA}</b> é maior ou igual que <b>${valorB}</b>.Repita a operação.`
     
-    formIsValid = validarCampo(valorA.value, valorB.value)
+    const formIsValid = validarCampo(valorA, valorB);
 
     if (formIsValid) {
-        alert(mensagemSucesso);
-    }else if (valorA > valorB){
-        alert("A é maior que B!");
-    }else {
-        alert("A é igual a B!");
-    }
-})
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+        
+        
 
-console.log(form);
+    }else {
+        const containerMesagemErro = document.querySelector('.error-message');
+        containerMesagemErro.innerHTML = mensagemErro
+        containerMesagemErro.style.display = 'block';
+        
+        campoA.value = '';
+        campoB.value = '';
+    }
+    
+        
+});
+
+
